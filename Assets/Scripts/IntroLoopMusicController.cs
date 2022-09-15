eb84b7cfc262bf56a9c639540b8439daf04f4e8a
Sammot <45080494+Sammot@users.noreply.github.com>
2022-09-15 17:06:57 +0100

    Started adding music, menu is almost done

---
+++ Assets/Scripts/IntroLoopMusicController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroLoopMusicController : MonoBehaviour
+{
+    [SerializeField] AudioSource IntroSource;
+    [SerializeField] AudioSource LoopSource;
+
+    private void Awake()
+    {
+        IntroSource.Play();
+        Invoke("StartLoopSection", IntroSource.clip.length);
+
+        StartCoroutine(StartLoopSection());
+    }
+
+    private IEnumerator StartLoopSection()
+    {
+        yield return new WaitForSeconds(IntroSource.clip.length - 0.6f);
+        LoopSource.Play();
+    }
+}
