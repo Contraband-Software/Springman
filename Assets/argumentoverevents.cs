50e2f098c9b29ea8bb3897951477345ae8c3a3e3
CYANSERAPH <samtaseff@gmail.com>
2022-09-22 14:13:06 +0100

    Moved skin previewing to a non-build (editor-only) gameobject

--- Assets/argumentoverevents.cs
+++ Assets/argumentoverevents.cs
@@ -1,36 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Events;
 
-public class argumentoverevents : MonoBehaviour
-{
-    public class objecty
-    {
-        public string name;
-    }
-    public class MyEvent : UnityEvent<objecty, string>
-    {
+//public class argumentoverevents : MonoBehaviour
+//{
+//    public class objecty
+//    {
+//        public string name;
+//    }
+//    public class MyEvent : UnityEvent<objecty, string>
+//    {
 
-    }
+//    }
 
-    public MyEvent daEvent = new MyEvent();
-    public objecty theObject = new objecty();
+//    public MyEvent daEvent = new MyEvent();
+//    public objecty theObject = new objecty();
 
-    private void Start()
-    {
-        daEvent.AddListener((objecty one, string two) =>
-        {
-            Debug.Log(one.ToString() + " " + two);
-        });
+//    private void Start()
+//    {
+//        daEvent.AddListener((objecty one, string two) =>
+//        {
+//            Debug.Log(one.ToString() + " " + two);
+//        });
 
-        StartCoroutine(ExecuteAfterTime(10f));
-    }
+//        StartCoroutine(ExecuteAfterTime(10f));
+//    }
 
-    IEnumerator ExecuteAfterTime(float time)
-    {
-        yield return new WaitForSeconds(time);
+//    IEnumerator ExecuteAfterTime(float time)
+//    {
+//        yield return new WaitForSeconds(time);
 
-        daEvent.Invoke(theObject, "hello");
-    }
-}
+//        daEvent.Invoke(theObject, "hello");
+//    }
+//}
