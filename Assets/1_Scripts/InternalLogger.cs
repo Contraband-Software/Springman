4350b5e124255cc02d2f069823eb18fa811d143d
cyanseraph <sam@cyanseraph.net>
2022-11-04 02:36:29 +0000

    Internal logger to make logcat output easier to read

---
+++ Assets/1_Scripts/InternalLogger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class InternalLogger
+{
+    private static ILogger logger = Debug.unityLogger;
+    private static string kTag = "CONTRABAND";
+
+    public static void Log(string message)
+    {
+        logger.Log(kTag, message);
+    }
+
+}
