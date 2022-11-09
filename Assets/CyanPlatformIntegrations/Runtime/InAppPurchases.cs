be4c12363cb0a49b7f2817d8ad0af95c626a4c80
cyanseraph <sam@cyanseraph.net>
2022-11-09 15:08:09 +0000

    Created iap store

--- Assets/CyanPlatformIntegrations/Runtime/InAppPurchases.cs
+++ Assets/CyanPlatformIntegrations/Runtime/InAppPurchases.cs
@@ -1,23 +1,141 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Purchasing;
 
 namespace PlatformIntegrations
 {
-    public class InAppPurchases : MonoBehaviour
+    using System;
+    using UnityEngine.Events;
+    using UnityEngine.Purchasing;
+    using Unity.Services.Core;
+    using Unity.Services.Core.Environments;
+
+    public class InAppPurchases : MonoBehaviour, IStoreListener
     {
-        private void Start()
+        const string logDecorator = "IAP: ";
+
+#region EVENTS
+
+        public class OnInitialize : UnityEvent<bool> { }
+        [HideInInspector] public OnInitialize OnInitializeEvent;
+        public class OnPurchaseSuccess : UnityEvent<PurchaseEventArgs> { }
+        [HideInInspector] private OnPurchaseSuccess OnPurchaseSuccessEvent;
+        public class OnPurchaseFail : UnityEvent<Product, PurchaseFailureReason> { }
+        [HideInInspector] private OnPurchaseFail OnPurchaseFailEvent;
+
+#endregion
+
+        //store
+        private IStoreController controller;
+        private IExtensionProvider extensions;
+
+        private Dictionary<string, Func<bool, PurchaseFailureReason, PurchaseEventArgs, PurchaseProcessingResult>> purchaseProcessingCallbacks;
+
+        private string environment = "production";
+
+        private async void Awake()
         {
-            //ProductCatalog.LoadDefaultCatalog().allProducts
-            Debug.Log("IAP INIT: ");
-            Debug.Log(CodelessIAPStoreListener.initializationComplete);
+            OnInitializeEvent = new OnInitialize();
+            OnInitializeEvent.AddListener((bool status) =>
+            {
+                Debug.Log(logDecorator + "Initialized: " + status.ToString());
+            });
+            OnPurchaseSuccessEvent = new OnPurchaseSuccess();
+            OnPurchaseSuccessEvent.AddListener((PurchaseEventArgs args) =>
+            {
+                Debug.Log(logDecorator + "Successfully purchased: " + args.purchasedProduct.definition.id);
+            });
+            OnPurchaseFailEvent = new OnPurchaseFail();
+            OnPurchaseFailEvent.AddListener((Product product, PurchaseFailureReason reason) =>
+            {
+                Debug.Log(logDecorator + "Failed to purchase: " + product.definition.id + ": " + reason.ToString());
+            });
+
+            //UNITY GAMING SERVICES
+            try 
+            {
+                var options = new InitializationOptions()
+                    .SetEnvironmentName(environment);
+
+                await UnityServices.InitializeAsync(options);
+
+                Debug.Log(logDecorator + "INITALIZED UNITY GAMING SERVICES");
+
+                InitializeIAP();
+            }
+            catch (Exception exception)
+            {
+                Debug.Log(logDecorator + "FAILED TO INITIALIZE UNITY GAMING SERVICES, IAP ABORTED");
+            }
         }
 
-        // Update is called once per frame
-        void Update()
+        private void InitializeIAP()
         {
+            ConfigurationBuilder builder = ConfigurationBuilder.Instance(StandardPurchasingModule.Instance());
 
+            //Use the codeless IAP catalogue
+            ProductCatalog pc = ProductCatalog.LoadDefaultCatalog();
+            foreach (ProductCatalogItem item in pc.allProducts)
+            {
+                builder.AddProduct(item.id, item.type);
+            }
+
+            UnityPurchasing.Initialize(this, builder);
         }
+
+#region ISTORELISTENER_IMPLEMENTATION
+
+        public void OnInitialized(IStoreController controller, IExtensionProvider extensions)
+        {
+            this.controller = controller;
+            this.extensions = extensions;
+
+            OnInitializeEvent.Invoke(true);
+        }
+
+        public void OnInitializeFailed(InitializationFailureReason error)
+        {
+            OnInitializeEvent.Invoke(false);
+        }
+
+        public PurchaseProcessingResult ProcessPurchase(PurchaseEventArgs purchaseEvent)
+        {
+            //OnPurchaseSuccessEvent.Invoke(purchaseEvent);
+            return purchaseProcessingCallbacks[purchaseEvent.purchasedProduct.definition.id](true, PurchaseFailureReason.Unknown, purchaseEvent);//PurchaseProcessingResult.Complete;
+        }
+
+        public void OnPurchaseFailed(Product product, PurchaseFailureReason failureReason)
+        {
+            //OnPurchaseFailEvent.Invoke(product, failureReason);
+            purchaseProcessingCallbacks[product.definition.id](false, failureReason, null);
+        }
+
+#endregion
+
+#region PUBLIC_INTERFACE
+
+        /// <summary>
+        /// Registers a function to handle the purchase for a specific product ID, call this on any script handling a purchase for a product in Start().
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <param name="callback">This function will handle both successful purchases and failiures: bool success?, PurchaseFailureReason (Unknown if successful), PurchaseEventArgs AllTheDataYouNeedAboutTheProduct</param>
+        /// <exception cref="Exception">If the function fails to add to the dictionary for any reason</exception>
+        public void RegisterPurchaseProcessor(string productID, Func<bool, PurchaseFailureReason, PurchaseEventArgs, PurchaseProcessingResult> callback)
+        {
+            if (!purchaseProcessingCallbacks.TryAdd(productID, callback)) {
+                throw new Exception(logDecorator + "PURCHASE PROCESSING CALLBACK FAILED TO REGISTER");
+            }
+        }
+
+        /// <summary>
+        /// YOU MUST REGISTER A PURCHASE PROCESSOR CALLBACK FOR ANY PRODUCT ID USED WITH THIS METHOD.
+        /// </summary>
+        /// <param name="productID">The product ID defined in the IAP catalogue</param>
+        public void InitiatePurchase(string productID)
+        {
+            controller.InitiatePurchase(productID);
+        }
+
+#endregion
     }
 }
