80fc6c75c19f1a07a6e0413e359e1080e1af2214
cyanseraph <sam@cyanseraph.net>
2022-11-10 15:29:51 +0000

    Refactored GDC, made menumusic not need destroy duplicate, removed GDC tag

--- Assets/1_Scripts/Sound/MenuAudioManager.cs
+++ Assets/1_Scripts/Sound/MenuAudioManager.cs
@@ -4,8 +4,19 @@ using UnityEngine;
 
 public class MenuAudioManager : MonoBehaviour
 {
+    public static bool instanced = false;
+
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+
+        if (!instanced)
+        {
+            instanced = true;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
